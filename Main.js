8ed56357448ba5c387f56ab1866909c06630961e
leone <leonemunyao>
2023-03-12 15:43:43 +0300

    First Commit

---
+++ Main.js
@@ -0,0 +1,26 @@
+// Javascript has 8 datatypes
+//They are String Number Boolean Undefined Null Symbol Object Bigit
+//Object datatype can contain an array, object, date
+
+//Numbers
+let length = 16;
+let weight = 7.5;
+
+//Strings
+let color = Green;
+let FirstName = Leon;
+
+//Booleans
+let x = true;
+let y = false;
+
+//Object
+const name = {FirstName:"Leone", LastName:"Munyao"};
+
+//Array
+const cars = ["Mercedes", "Landcruiser V8", "Audi Q7"];
+
+//Date objects
+const dates = new Date ("2023.03.12");
+
+//The concept of DataTypes
